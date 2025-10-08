3817256a6f1e5565660bbb8be4e4e40342ae9406
Isabelle <isabelle.dahlstromm@gmail.com>
2025-10-08 11:35:13 +0200

    refactor: remove commented-out test functions in test_dice.py

--- test/test_dice.py
+++ test/test_dice.py
@@ -2,22 +2,7 @@ import pytest
 import unittest
 from src.dice import Dice
 
-# def test_roll_returns_int():
-#     dice = Dice()
-#     result = dice.roll()
-#     assert isinstance(result, int), "Roll should return an integer"
 
-# def test_roll_in_range():
-#     dice = Dice()
-#     for _ in range(100):  # roll multiple times
-#         result = dice.roll()
-#         assert 1 <= result <= 6, "Roll should be between 1 and 6"
-
-# def test_custom_sides():
-#     dice = Dice(num_sides=20)
-#     for _ in range(100):
-#         result = dice.roll()
-#         assert 1 <= result <= 20, "Roll should be within the custom number of sides"
 
 class Test_Dice_Roll(unittest.TestCase):
 
