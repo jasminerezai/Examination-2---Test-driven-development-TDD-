a678ed80e4ac95fe2928239a1a760f2a0cd020cd
Isabelle <isabelle.dahlstromm@gmail.com>
2025-10-10 09:41:29 +0200

    refactor: clean up Dice class and improve roll method functionality

--- src/dice.py
+++ src/dice.py
@@ -1,24 +1,13 @@
 import random
 
-# This class has one method, roll(). 
-# The class implements a dice with 6 sides.
-# The method then rolls the dice and it will result in a number beteween 1-6.
-# The number gets returned.
-
-# class Dice:
-#     def __init__(self, num_sides=6):
-#         self.num_sides = num_sides
-
-#     def roll(self):
-#         return random.randint(1, self.num_sides)
-
-
-
 class Dice:
-
-    def __init__(self):
-           random.seed()
+    def __init__(self, num_sides=6):
+        self.num_sides = num_sides
+        self.value = 1  # default starting value, can be whatever you prefer
 
     def roll(self):
-        
-        return random.randint(1, 6)
+        self.value = random.randint(1, self.num_sides)
+        return self.value
+
+    def __str__(self):
+        return str(self.value)
